268308a09a42938826613842c7f92d7e9c8aad39
n5ssim <nassimc02@gmail.com>
2025-11-27 17:05:20 +0100

    ptite modif

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: n5ssim <nchouaf@student.s19.be>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:22:27 by n5ssim            #+#    #+#             */
-/*   Updated: 2025/11/26 16:28:03 by n5ssim           ###   ########.fr       */
+/*   Updated: 2025/11/27 17:04:38 by n5ssim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,5 @@ char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s);
 
 #endif
